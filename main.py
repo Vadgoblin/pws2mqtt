539ae2b78297b4279b69126b6f3fabc6abc0ea43
vadgoblin@arch <vadgoblin@github.com>
2026-09-17 16:06:11 +0200

    add payload processing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+from typing import Any
 
 import uvicorn
 from fastapi import FastAPI, Response, status
@@ -13,17 +14,36 @@ app = FastAPI()
 async def update_weather_station(request: Request):
     data = WeatherStationPayload(**request.query_params)
 
-    # Example verification
-    # if data.ID != "expected_id" or data.PASSWORD != "expected_password":
-    #     return Response(content="INVALIDCREDENTIALS\n", media_type="text/plain", status_code=status.HTTP_401_UNAUTHORIZED)
 
     # Process  the data
-    station_payload = data.model_dump()
-    print(json.dumps(station_payload, indent=2))
+    # station_payload = data.model_dump()
+    processed_payload = serialize_payload(data)
+    processed_payload = round_floats(processed_payload)
+    print(json.dumps(processed_payload, indent=2))
 
     # Weather Underground-compatible stations expect 'success\n' in plain text
     return Response(content="success\n", media_type="text/plain", status_code=status.HTTP_200_OK)
 
+
+def serialize_payload(data: WeatherStationPayload):
+    processed_data = {
+        "station_id": data.station.station_id,
+        "indoor": data.indoor.model_dump(),
+        "outdoor": data.outdoor.model_dump(),
+        "channels": data.channels.model_dump()
+    }
+
+    return processed_data
+
+def round_floats(obj: Any, decimals: int = 2) -> Any:
+    """Recursively traverse a dictionary or list and round all float values."""
+    if isinstance(obj, float):
+        return round(obj, decimals)
+    elif isinstance(obj, dict):
+        return {k: round_floats(v, decimals) for k, v in obj.items()}
+    elif isinstance(obj, list):
+        return [round_floats(item, decimals) for item in obj]
+    return obj
 
 uvicorn.run(
     app,
