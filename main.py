7c86bdde5d6d622d04740ec56c47141dc0fb8c66
vadgoblin@arch <vadgoblin@github.com>
2026-09-17 12:37:25 +0200

    not sure what to do

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ from typing import Optional
 
 import uvicorn
 from fastapi import FastAPI, Depends, Response, status
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
+from starlette.requests import Request
 
 app = FastAPI()
 
 
 class WeatherStationData(BaseModel):
+    model_config = ConfigDict(extra="allow")
+
     # Credentials and metadata
     ID: str
     PASSWORD: str
@@ -30,21 +33,26 @@ class WeatherStationData(BaseModel):
     solarradiation: Optional[float] = None
     UV: Optional[float] = None
 
-    # Indoor & soil sensors
+    # Indoor
     indoortempf: Optional[float] = None
     indoorhumidity: Optional[int] = None
-    soiltempf: Optional[float] = None
-    soilmoisture: Optional[int] = None
+
+
+    # soiltempf: Optional[float] = None
+    # soilmoisture: Optional[int] = None
 
 
 @app.get("/weatherstation/updateweatherstation.php")
-async def update_weather_station(data: WeatherStationData = Depends()):
+async def update_weather_station(request: Request):
+    data = WeatherStationData(**request.query_params)
+
     # Example verification
     # if data.ID != "expected_id" or data.PASSWORD != "expected_password":
     #     return Response(content="INVALIDCREDENTIALS\n", media_type="text/plain", status_code=status.HTTP_401_UNAUTHORIZED)
 
     # Process  the data
     station_payload = data.model_dump()
+    print(station_payload)
     print(json.dumps(station_payload, indent=2))
 
     # Weather Underground-compatible stations expect 'success\n' in plain text
